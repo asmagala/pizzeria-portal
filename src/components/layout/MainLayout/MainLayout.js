e89462e6a46cc9eb340032d92bdcf6a4a718a732
Arkadiusz Smagała <asmagala@outlook.com>
2020-07-19 23:14:44 +0200

    Create main subpages and PageNav.

---
+++ src/components/layout/MainLayout/MainLayout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PageNav from '../PageNav/PageNav';
+import PropTypes from 'prop-types';
+
+const MainLayout = ({children}) => {
+  return(
+    <div>
+      <PageNav />
+      {children}
+    </div>
+  );
+};
+
+MainLayout.propTypes = {
+  children: PropTypes.node,
+};
+
+export default MainLayout;
